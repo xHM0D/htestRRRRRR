2a0a7b30b3de9ab54d477a0179a850f397118dcf
hm0d <106934512+xHM0D@users.noreply.github.com>
2024-04-24 20:59:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const app = express();
+const app = express()
+const port = process.env.PORT || 4000;
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+  res.send("Hello World")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 app.listen(3000);
 //كود تثبيت حسابك ٢٤ ساعة في روم فويس
