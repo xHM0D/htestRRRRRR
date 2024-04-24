023cd49bf1e4eafe3c94bb7eb9cb913d05b09cc6
hm0d <106934512+xHM0D@users.noreply.github.com>
2024-04-24 21:08:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require("express");
-const app = express()
-const port = process.env.PORT || 4000;
+const app = express();
 
 app.get("/", function (req, res) {
   res.send("Hello World")
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
 
 app.listen(3000);
