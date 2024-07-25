1ce90c0fe2387691897e7707b1c3d83488500b38
hm0d <106934512+xHM0D@users.noreply.github.com>
2024-07-25 20:48:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World")
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({checkUpdate:false});
 
