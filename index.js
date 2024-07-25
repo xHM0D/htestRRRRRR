86f7574bd053b5d36637f9cdc0e6a22d4ec3920c
hm0d <106934512+xHM0D@users.noreply.github.com>
2024-07-25 20:51:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", function (req, res) {
+  res.send("Hello World")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+app.listen(3000);
 const { Client } = require('discord.js-selfbot-v13');
-const client = new Client({checkUpdate:false});
+const client = new Client({checkUpdate:false}); 
 
-client.on('ready', async () => {
+setInterval(() => {
+  if (!client || !client.user) {
+    console.log("Client not login");
+    console.log("Restart project");
+    process.kill(1);
+  }
+}, 1000);
+
+client.on("ready", async () => {
   console.log(`${client.user.username} is ready!`);
-})
+});
 //ثبات فويس 24 ساعه v13 بدون اي مشاكل
-const { joinVoiceChannel } = require('@discordjs/voice');
-client.on('ready', () => {
-    
-    setInterval( async () => {
-    client.channels.fetch(process.env.channel) 
-     .then((channel) => { 
-      const VoiceConnection = joinVoiceChannel({
-       channelId: channel.id, 
-       guildId: process.env.guild, 
-       selfMute: true,
-       selfDeaf: true,
-       adapterCreator: channel.guild.voiceAdapterCreator 
-       });
-    }).catch((error) => { return; });
-    }, 1000)
-}); 
-//https://ra3dstudio.com CopyRight Codes
+const { joinVoiceChannel } = require("@discordjs/voice");
+client.on("ready", () => {
+  setInterval(async () => {
+    client.channels
+      .fetch(process.env.channel)
+      .then((channel) => {
+        const VoiceConnection = joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          selfMute: true,
+          selfDeaf: true,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+      })
+      .catch((error) => {
+        return;
+      });
+  }, 1000);
+});
 client.login(process.env.token);
