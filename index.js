5d78bdd7acc2344c2ac214922b16a1ac23de06a5
hm0d <106934512+xHM0D@users.noreply.github.com>
2024-07-26 10:55:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,43 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const { Client, GatewayIntentBits } = require("discord.js");
-const { joinVoiceChannel } = require("@discordjs/voice");
-require("web-streams-polyfill/ponyfill"); // استيراد web-streams-polyfill
-const fetch = require("node-fetch"); // استيراد node-fetch
 
-// إعداد خادم Express
 app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+  res.send("Hello World")
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(3000);
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({checkUpdate:false}); 
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
-});
-
-client.on("debug", console.log);
-client.on("warn", console.warn);
-client.on("error", console.error);
+setInterval(() => {
+  if (!client || !client.user) {
+    console.log("Client not login");
+    console.log("Restart project");
+    process.kill(1);
+  }
+}, 1000);
 
 client.on("ready", async () => {
   console.log(`${client.user.username} is ready!`);
-  console.log("token:", process.env.token);
-  console.log("channel:", process.env.channel);
 });
-
-// هنا يمكن استخدام fetch لجلب البيانات
-fetch("https://api.example.com/data")
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error("Error fetching data:", err));
-
+//ثبات فويس 24 ساعه v13 بدون اي مشاكل
+const { joinVoiceChannel } = require("@discordjs/voice");
 client.on("ready", () => {
   setInterval(async () => {
     client.channels
       .fetch(process.env.channel)
       .then((channel) => {
-        json;
         const VoiceConnection = joinVoiceChannel({
           channelId: channel.id,
-          guildId: channel.guild.id,
+          guildId: process.env.guild,
           selfMute: true,
           selfDeaf: true,
           adapterCreator: channel.guild.voiceAdapterCreator,
         });
       })
       .catch((error) => {
-        console.error("Error joining voice channel:", error);
+        return;
       });
   }, 1000);
 });
-
-client.login(process.env.token).catch((error) => {
-  console.error("Error logging in:", error);
-  process.exit(1);
-});
+client.login(process.env.token);
